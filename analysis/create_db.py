f0a49fbb5049bdf6e279e698fd2d20ca3e23a471
Antonio Pierro <antonio.pierro@gmail.com>
2017-08-05 14:52:16 +0200

    first commit of analysis package

---
+++ analysis/create_db.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Numeric, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class Person (Base):
+    __tablename__ = 'person'
+    # Here we define columns for the table person
+    id = Column(Integer, primary_key = True)
+    hand = Column(String(1), nullable = False)
+    gender = Column(String(1), nullable = False)
+    age = Column(Integer, nullable = False)
+    # The value 1 means the user is reliable, otherwise it will be 0
+    isValid = Column(Boolean, default = 1)
+    # The file name from where the data are taken
+    file_name = Column(String(250), nullable = False)
+
+
+# Create an engine that stores data in the local directory's
+# sqlalchemy_example.db file.
+engine = create_engine('sqlite:///arguments.db')
+
+# Create all tables in the engine. This is equivalent to "Create Table"
+# statements in raw SQL.
+Base.metadata.create_all(engine)
