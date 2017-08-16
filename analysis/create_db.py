40d47056b4966872f71735fc9682c2f83eec288b
Antonio Pierro <antonio.pierro@gmail.com>
2017-08-16 10:40:09 +0200

    Updated table's columns

--- analysis/create_db.py
+++ analysis/create_db.py
@@ -7,28 +7,28 @@ from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-
 class Person (Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
     id = Column(Integer, primary_key = True)
     age = Column(Integer, nullable = False)
-    hand = Column(String(1), nullable = False)
-    # TODO fix enum type in sqlalchemy
-    gender = Column(Enum('M', 'F'), nullable = False)
-    # The file name from where the data are taken
-    file_name = Column(String(250), nullable = False)
+    hand = Column(String(1), nullable = False) # D-S
+    gender = Column(String(1), nullable = False) # TODO fix ENUM type in sqlalchemy - M-F
+    file_name = Column(String(250), nullable = False) # The file name from where the data are taken
 
 class Sentence (Base):
     __tablename__ = 'sentence'
-    # Here we define columns for the table person
     id = Column(Integer, primary_key = True)
     phrase = Column(String(250), nullable = False)
+    gender = Column(String(1), nullable = False) # M-F
+    target_word_n = Column(String(20), nullable = False)
+    target_word_p = Column(String(20), nullable = False)
+    sentence_block = Column(String(5), nullable = False) # PB-T-DISTR
+    context = Column(String(1), nullable = False) # P-N
 
 # Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
-engine = create_engine('sqlite:///irony.db')
+file_name = 'irony.db'
+os.remove(file_name) if os.path.exists(file_name) else None
+engine = create_engine('///'.join(['sqlite:', file_name]))
 
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
+# Create all tables in the engine. This is equivalent to "Create Table" statements in raw SQL.
 Base.metadata.create_all(engine)
